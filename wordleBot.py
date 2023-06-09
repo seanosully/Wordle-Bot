f7e077bfd0580df60a383090fcc06416629a9860
seanosully <sean13.osullivan@gmail.com>
2023-06-09 15:12:41 -0400

    Wordle Bot v1.1
    Updated the bot to use filler words if left with multiple very similar possible solutions

--- wordleBot.py
+++ wordleBot.py
@@ -1,4 +1,4 @@
-## Wordle Bot 1.0
+## Wordle Bot v1.1
 ## Sean O'Sullivan
 ## 06-02-2023
 from itertools import permutations
@@ -13,42 +13,41 @@ def read_file(file):
         wordList.append(word[:5])
     return wordList
 
-# Add letters that aren't in the word to the forbidden letter set
-def add_forbidden_letters(guess, pattern):
+# Add forbidden letters to set
+def add_forbidden_letters(guess, pattern, forbidden):
     for i, letter in enumerate(guess):
         if pattern[i] < 1:
             forbidden.add(letter)
     return forbidden
 
-# Calculate the current score of a guess. This is only used when the answer word is known
-def calculate_score(guess, answer):
-    pattern = [0] * 5
-    used1 = [0] * 5
-    used2 = [0] * 5
-
-    for i, letter in enumerate(guess):
-        forbidden.add(letter)
-        if answer[i] == letter:
-            pattern[i] = 2
-            used1[i], used2[i] = 1, 1
-            if letter in forbidden:
-                forbidden.remove(letter)
-    for a, l1 in enumerate(answer):
-        if used1[a] == 1:
+# Calculate the score of the computed guess
+def calc_score(guess, answer):
+    score = [0] * 5
+    used_guess = [False] * 5
+    used_answer = [False] * 5
+    for i, g in enumerate(guess):
+        forbidden.add(g)
+        if answer[i] == g:
+            score[i] = 2
+            used_answer[i], used_guess[i] = True, True
+        if g in forbidden:
+                forbidden.remove(g)
+    for i, a in enumerate(answer):
+        if used_answer[i] == True:
             continue
         else:
-            for b, l2 in enumerate(guess):
-                if used2[b] == 1:
+            for j, g in enumerate(guess):
+                if used_guess[j]:
                     continue
-                elif l1 == l2:
-                    used1[a], used2[b] = 1, 1
-                    pattern[b] = 1
-                    if l2 in forbidden:
-                        forbidden.remove(l2)
+                elif a == g:
+                    score[j] = 1
+                    used_answer[i], used_guess[j] = True, True
+                    if g in forbidden:
+                        forbidden.remove(g)
                     break
-    return pattern
+    return score
 
-# Add patterns to a set of patterns to avoid. This is to avoid placing letters in spots where they were previously yellow
+# Create patterns that the bot should avoid guessing and add them to the set of avoided patterns
 def avoided_patterns(pattern, guess, avoiding):
     avoid = ''
     for i, letter in enumerate(guess):
@@ -58,7 +57,7 @@ def avoided_patterns(pattern, guess, avoiding):
             avoid += '@'
     avoiding.add(avoid)
 
-# Keep track of letters that are in the correct spot
+# Create a list of letters that are known to be in the word and return them
 def make_combos(pattern, guess):
     locks = ["@", "@", "@", "@", "@"]
     for i, correct in enumerate(pattern):
@@ -66,7 +65,7 @@ def make_combos(pattern, guess):
             locks[i] = guess[i]
     return locks
 
-# Make a set of all possible combos of the known letters
+# Compute all possible permutations the known letters of the word
 def compute_perms(locks, pattern):
     perms2 = set()
     perms = [''.join(p) for p in permutations(locks)]
@@ -83,8 +82,28 @@ def compute_perms(locks, pattern):
                 break
         if add:
             perms2.add((perm,0))
-
     return [list(p) for p in perms2]
+
+# Combine the permutaions of the previous and filler guess to create more specific permutations
+def combine_perms(perms, fillerPerms, pattern):
+    newPerms = []
+    for perm in perms:
+        for fillerPerm in fillerPerms:
+            add = True
+            newPerm = ''
+            for i in range(5):
+                if perm[0][i] != '@' and fillerPerm[0][i] != '@' and perm[0][i] != fillerPerm[0][i]:
+                    add = False
+                    break
+                elif perm[0][i] != '@':
+                    newPerm += perm[0][i]
+                elif fillerPerm[0][i] != '@':
+                    newPerm += fillerPerm[0][i]
+                else:
+                    newPerm += '@'
+            if add:
+                newPerms.append([newPerm, 0])
+    return newPerms
 
 # Update the list by removing words that have letters in spots that were previously yellow
 def updateList1(wordList, avoidlist):
@@ -153,55 +172,87 @@ def next_guess(wordList, perm):
         if add:
             return word
 
-# Solve the wordle with a known answer
-def solve(wordList, ans):
-    guess = "salet"
-    count = 0
-    forbidden = set()
-    
-    while guess != ans:
-        count = count + 1
-        print("Num Guesses: " + str(count))
-        print("GUESS:  " + guess + "   ANS:   " + ans)
-        pattern = calculate_score(guess, ans)
-        locks = make_combos(pattern, guess)
-        p = compute_perms(locks, pattern)
-        avoiding =  set()
-        avoided_patterns(pattern, guess, avoiding)
-        wordList = updateList1(wordList, avoiding)
-        forbidden = add_forbidden_letters(guess, pattern)
-        wordList = updateList3(wordList, forbidden, p)
-        wordList, permToGuess = updateList2(wordList, p, guess)
-        print(str(len(wordList)) + " possible words")
-        if(len(wordList) == 0):
-            print("No such word exists in the dictionary")
-            return count
-        guess = next_guess(wordList, permToGuess)
-        if gameType != 't':
-            print()
-            print("Press Enter to Continue")
-            x = input()
-    count = count + 1
+# Return a filler word that will help narrow down the list of words that all have the same known permutation
+def filler_word(wordList, fullList, locks):
+    charCount = [0] * 26
+    for word in wordList:
+        for i in range(len(word)):
+            if locks.count(word[i]) == 0:
+                charCount[ord(word[i]) - 97] += 1
+    maxLetters = 0
+    topWord = ""
+    for word in fullList:
+        curr = 0
+        for i, char in enumerate(charCount):
+            if char > 0 and word.count(chr(i + 97)) and locks.count(char) == 0:
+                curr += 1
+        if curr > maxLetters:
+            topWord = word
+            maxLetters = curr
+    return topWord
 
-    print()
-    print("Solution:  " + guess)
-    print()
-    print("SOLUTION TOOK " + str(count) + " guesses.")
-    print()
-    return count
-    
 # Calculate the average turns the bot takes to reach the answer
-def compute_avg_guessesiciency(wordList):
+def compute_avg_guesses(wordList):
     total = 0
     num = len(wordList)
     newList = wordList
     for ans in wordList:
         print(len(newList))
-        count = solve(newList, ans)
+        count = solve_known(newList, ans)
         newList = wordList
         forbidden.clear()
         total += count
     return total / num
+
+# Solve the wordle with a known answer
+def solve_known(wordList, answer):
+    locks = ["@", "@", "@", "@", "@"]
+    count = 0
+    forbidden = set()
+    guess = "salet"
+    filler = False
+    perms1 = ["@@@@@"]
+    while guess != answer:
+        count = count + 1
+        print()
+        print("Num Guesses: " + str(count))
+        print("GUESS:  " + guess + "   ANS:   " + answer)
+        print()
+        pattern = calc_score(guess, answer)
+        locks = make_combos(pattern, guess)
+        print(pattern)
+        if not filler:
+            perms1 = compute_perms(locks, pattern)
+            perm = perms1
+        else:
+            perms2 = compute_perms(locks, pattern)
+            perm = combine_perms(perms1, perms2, pattern)
+        avoiding =  set()
+        avoided_patterns(pattern, guess, avoiding)
+        wordList = updateList1(wordList, avoiding)
+        forbidden = add_forbidden_letters(guess, pattern, forbidden)
+        wordList = updateList3(wordList, forbidden, perm)
+        wordList, permToGuess = updateList2(wordList, perm, guess)
+        perms1 = perm
+        print(str(len(wordList)) + " possible words")
+        if(len(wordList) == 0):
+            print("No such word exists in the dictionary")
+            return count
+        guess = next_guess(wordList, permToGuess)
+        newGuess = filler_word(wordList, fullList, locks)
+        filler = False
+        if len(perm) < 2 and perm[0][1] > 2 or (len(perm) == 2 and (perm[1][1] < 2 and perm[0][1] > 2)) or (len(perm) == 2 and (perm[0][1] < 2 and perm[1][1] > 2)):
+            guess = newGuess
+            filler = True
+        if gameType != 't':
+            wait = input()
+    count += 1
+    print()
+    print("Solution is: " + guess)
+    print()
+    print("Solution took " + str(count) + " guesses.")
+    print()
+    return count
 
 # Solve for an unknown word
 def solve_unknown(wordList):
@@ -209,38 +260,68 @@ def solve_unknown(wordList):
     guess = "salet"
     pattern = [0,0,0,0,0]
     avoiding = set()
+    forbidden = set()
+    filler = False
+    inputx = ""
     while pattern != [2,2,2,2,2]:
         count = count+1
         print("Num Guesses: " + str(count))
         print("GUESS:  " + guess + "   ANS:   ?")
         print()
-        print("Enter the guess feedback (0 for grey, 1 for yellow, and 2 for green)")
-        print("Example: 20011 would be the input for - green grey grey yellow yellow")
-        inputx = input()
+        while(inputx.count('0') + inputx.count('1') + inputx.count('2') != 5 or len(inputx) != 5):
+            print("Enter the guess feedback (0 for grey, 1 for yellow, and 2 for green)")
+            print("Example: 20011 would be the input for - green grey grey yellow yellow")
+            inputx = input()
         pattern = [int(inputx[0]), int(inputx[1]), int(inputx[2]), int(inputx[3]), int(inputx[4])]
+        if pattern == [2,2,2,2,2]:
+            break
         locks = make_combos(pattern, guess)
-        p = compute_perms(locks, pattern)
+        if not filler:
+            perms1 = compute_perms(locks, pattern)
+            print("PERM: ")
+            print(perms1)
+            perm = perms1
+        else:
+            perms2 = compute_perms(locks, pattern)
+            perm = combine_perms(perms1, perms2, pattern)
+            print(perm)
+        avoiding =  set()
         avoided_patterns(pattern, guess, avoiding)
         wordList = updateList1(wordList, avoiding)
-        forbidden = add_forbidden_letters(guess, pattern)
-        wordList = updateList3(wordList, forbidden, p)
-        wordList, permToGuess = updateList2(wordList, p, guess)
+        forbidden = add_forbidden_letters(guess, pattern, forbidden)
+        print(forbidden)
+        wordList = updateList3(wordList, forbidden, perm)
+        wordList, permToGuess = updateList2(wordList, perm, guess)
+        print(wordList)
+        print(permToGuess)
+        perms1 = perm
         print(str(len(wordList)) + " possible words")
         if(len(wordList) == 0):
             print("No such word exists in the dictionary")
-            print()
             return count
         guess = next_guess(wordList, permToGuess)
-
-    print("Solution took " + str(count) + " GUESSES.")
+        newGuess = filler_word(wordList, fullList, locks)
+        filler = False
+        if len(perm) < 2 and perm[0][1] > 2 or (len(perm) == 2 and (perm[1][1] < 2 and perm[0][1] > 2)) or (len(perm) == 2 and (perm[0][1] < 2 and perm[1][1] > 2)):
+            guess = newGuess
+            filler = True
+    print()
+    print("Solution is:  " + guess)
+    print("Solution took " + str(count) + " guesses.")
+    print()
     return count
 
 # Main function
 def main():
+    global dictionary_file
+    global answers_file
     dictionary_file = sys.argv[1]
-    print("Wordle Bot")
-    print("Version 1.0")
-    print("This version solves wordle in an average of 3.8 move if the word list is limited to just possible wordle answers (2309 possible words) \nand an average of 4.904 when the word list is expanded to all 5 letter words (12972 possible words)")
+    answers_file = sys.argv[2]
+    print("Wordle Bot v1.1")
+    print("This version solves wordle in an average of:")
+    print("3.755 moves when the valid guess list is the full dictionary (12,972 words) and the valid solutions list is limited to valid wordle solutions (2,309 words).")
+    print("3.749 moves when the valid guess list is limited to valid wordle solutions and the valid solutions list is also limited to valid wordle solutions.")
+    print("4.567 moves when the valid guess list is the full dictionary list and the valid solutions list is also the full dictionary list.")
     print()
     print("To solve a wordle where the answer is unknown type 'u' \nTo give the bot a word to solve type 'k' \nTo test the efficiency of the bot type 't'\nType 'q' to quit")
     global forbidden
@@ -249,15 +330,17 @@ def main():
     while gameType != 'q':
         ans = ""
         forbidden = set()
-        wordList = read_file(dictionary_file)
+        wordList = read_file(answers_file)
+        global fullList
+        fullList = read_file(dictionary_file)
         if gameType =='k':
             os.system('clear')
             while len(ans) != 5:
                 print("Enter a 5-letter word to solve:")
                 ans = input()
-            solve(wordList, ans)
+            solve_known(wordList, ans)
         elif gameType == 't':
-            print(compute_avg_guessesiciency(wordList))
+            print(compute_avg_guesses(wordList))
         elif gameType == 'u':
             os.system('clear')
             solve_unknown(wordList)
